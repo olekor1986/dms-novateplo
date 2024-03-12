0ec042561cc036de8050d15fb09a37488ede4cb5
olekor1986 <olekor1986@gmail.com>
2024-03-12 10:18:06 +0000

    Created CRUD for WaterMeter and WaterMeterValue

---
+++ database/migrations/2024_03_07_135206_create_water_meter_values_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('water_meter_values', function (Blueprint $table) {
+            $table->id();
+            $table->string('date');
+            $table->string('value');
+            $table->boolean('after_check')->nullable();
+            $table->string('note')->nullable();
+            $table->integer('water_meter_id');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('water_meter_values');
+    }
+};
